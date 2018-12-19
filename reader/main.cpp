153e1c6bd065d39a19499edb6b7fba8687e5268d
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-12-19 19:46:33 +0300

    Fix type for second step

--- reader/main.cpp
+++ reader/main.cpp
@@ -15,7 +15,7 @@ using TImprovedReader = TReader<
 using TBestReader = TReader<
     TTypeList<int, double, int>,
     TTypeList<TDecompressor<std::string, int>, TNone, TDecompressor<char, int>>,
-    TValueList<nullptr, Decompress<double, double>, Decompress<char, int>>
+    TValueList<nullptr, Decompress<double, double>, Decompress<int, int>>
 >;
 
 template <typename TReader>
